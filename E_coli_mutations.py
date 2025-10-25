0fcddbede07319f0131c19d223a5b118551c5768
deusmax <deusmaxbiz@gmail.com>
2025-10-25 12:24:20 +0200

    fix SymbolArray -> SymbolMap

--- E_coli_mutations.py
+++ E_coli_mutations.py
@@ -3,5 +3,5 @@ import replication
 with open("files/E_coli.txt", "r") as file:
     genome = file.read()
 
-C_counts = replication.SymbolArray(genome, "C")
+C_counts = replication.SymbolMap(genome, "C")
 print(C_counts)
