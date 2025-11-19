2d95476009973e46afa4044e3eb50844fe70afd2
deusmax <deusmaxbiz@gmail.com>
2025-11-19 15:35:53 +0100

    added Consensus and updated docstrings

--- motifs.py
+++ motifs.py
@@ -1,5 +1,5 @@
 """
-Count(Motifs)
+Count(Motifs: list[str])
 
 Return per-column counts of nucleotides for a list of equal-length DNA strings.
 
@@ -30,6 +30,8 @@ def Count(Motifs: list[str]):
     return count
 
 """
+Profile(Motifs: list[str])
+
 Compute the profile (column-wise nucleotide frequencies) for a set of DNA motifs.
 
 Args:
@@ -51,3 +53,30 @@ def Profile(Motifs: list[str]):
             lst[i] /= n
             lst[i] = round(lst[i], 2)
     return counts
+
+"""
+Consensus(Motifs: list[str])
+
+Return the consensus string for a list of equal-length DNA motifs.
+
+Args:
+    Motifs (list[str]): DNA strings of equal length using 'A', 'C', 'G', 'T'.
+Returns:
+    str: consensus sequence (most frequent nucleotide per column).
+Example:
+    >>> Consensus(["AT", "AC", "GT"])
+    'AT'
+"""
+def Consensus(Motifs: list[str]):
+    count = Count(Motifs)
+    k = len(Motifs[0])
+    consensus = ""
+    for i in range(k):
+        m = 0
+        mostFrequentSymbol = ""
+        for symbol in 'ACGT':
+            if count[symbol][i] > m:
+                m = count[symbol][i]
+                mostFrequentSymbol = symbol
+        consensus += mostFrequentSymbol
+    return consensus
