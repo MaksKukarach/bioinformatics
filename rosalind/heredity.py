c29808d46662330ce5adbcc2bfc3c22fc5fb6426
Maks <samoulov.max.18@gmail.com>
2026-03-18 18:28:56 +0100

    docstring

--- rosalind/heredity.py
+++ rosalind/heredity.py
@@ -3,11 +3,15 @@ def num_pairs(n):
 
 
 def prob_dominant(dom: int, het: int, rec: int):
+    """Given a sample with counts of homozygous dominant, heterozygous and homozygous recessive
+    organisms, return a probability that a random pair of organisms will produce and offspring
+    with a dominant allele."""
     total = num_pairs(dom + het + rec)
     p_both_dom = num_pairs(dom) / total
     p_both_het = (3 / 4) * (num_pairs(het) / total)
     p_dom_rec = (dom * rec) / total
     p_dom_het = (dom * het) / total
     p_het_rec = (1 / 2) * ((het * rec) / total)
+    # We use the law of total probability
     result = p_both_dom + p_both_het + p_dom_rec + p_dom_het + p_het_rec
     return round(result, 5)
